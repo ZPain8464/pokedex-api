c21935a3a4d4215ec775f75b536259557a5e6099
Zach Painter <z.painter45@gmail.com>
2020-10-07 21:27:29 -0500

    Pre deploy prep

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 
 const app = express();
 const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const validTypes = [
   `Bug`,
   `Dark`,
@@ -28,17 +29,18 @@ const validTypes = [
 ];
 const POKEDEX = require("./pokedex.json");
 
-app.use(morgan("dev"));
+// process.env.NODE_ENV
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "dev";
+
+app.use(morgan(morganSetting));
 app.use(cors());
 app.use(helmet());
 app.use(function validateBearerToken(req, res, next) {
   const authToken = req.get("Authorization");
   const apiToken = process.env.API_TOKEN;
 
-  console.log("validate bearer token middleware");
-
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
-    return res.status(401).json({ error: "Unauthorized request" });
+    return res.status(401).json({ error: "Unauthorized Request" });
   }
 
   next();
@@ -68,6 +70,16 @@ function handleGetPokemon(req, res) {
 
   res.json(response);
 }
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
